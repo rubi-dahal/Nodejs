3ffc023ea9e59d0a569d517d72e3f9f0acb167be
rubi-dahal <rrubidahal7@gmail.com>
2025-12-20 19:57:56 +0545

    a

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const connect = require("./DataBase/connection");
 const User = require("./models/userModel");
 const Blog = require("./models/BlogModel");
 const app = express();
+const bcrypt = require("bcrypt");
 
 connect();
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.json({
@@ -38,3 +40,52 @@ app.get("/fetch-blogs", async function (req, res) {
     data: data,
   });
 });
+
+app.post("/register", async function (req, res) {
+  //console.log(req.body); data will be in request.body
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  // const {name, email, password} = req.body
+  //console.log(name)
+  //console.log(password)
+  await User.create({
+    name: name,
+    email: email,
+    password: bcrypt.hashSync(password, 10),
+  });
+  res.json({
+    message: "User registered Successfully!!!",
+  });
+});
+
+app.post("/Post-blog", async function (req, res) {
+  const title = req.body.title;
+  const subtitle = req.body.subtitle;
+  const description = req.body.description;
+  await Blog.create({
+    title: title,
+    subtitle: subtitle,
+    description: description,
+  });
+  res.json({
+    message: "Blog Posted Successfully!!!",
+  });
+});
+
+app.delete("/delete-blog", async function (req, res) {
+  const id = req.body.id;
+  await Blog.findByIdAndDelete(id);
+  res.json({
+    message: "Blog with this id deleted Successfully!!!",
+  });
+});
+
+// app.put("/edit-blog", async function (req, res) {
+//   const id = req.body.id;
+//   await Blog.updateOne({ _id: id }, { title, content });
+
+//   res.json({
+//     message: "Blog with this id is updated Successfully!!!",
+//   });
+// });
